f7f9fbed1d59c23de425ff7515c04e13c41fefcb
Anuj <anujgajbhiye97@gmail.com>
2021-03-21 22:51:16 +0530

    ✨ Added episode list

--- src/components/AnimeList.vue
+++ src/components/AnimeList.vue
@@ -1,13 +1,34 @@
 <template>
-  <div></div>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div
+          v-for="(item, index) in alphabetList"
+          :key="index"
+          style="margin: 10px"
+        >
+          <router-link :to="'anime-list-' + item">
+            <strong>
+              {{ item }}
+            </strong>
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data: () => ({
+    alphabetList: [],
+  }),
 
-}
+  mounted() {
+    this.alphabetList = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+  },
+};
 </script>
 
-<style>
-
+<style scoped>
 </style>
--- src/components/WatchAnime.vue
+++ src/components/WatchAnime.vue
@@ -11,13 +11,25 @@
       <br>
       <strong>Anime Info: </strong><span>{{videoInfo.animeInfo}}</span>
       <hr>
+
+      <div class="row">
+        <div class="col-lg-6">
+          <a href="#">Previous</a>
+        </div>
+        <div class="col-lg-6" >
+          <a href="#" style="float: right">Next</a>
+        </div>
+      </div>
+
       
       <div class="row">
-        <div class="col-lg-1 col-md-1 col-sm-2 col-xs-2 col-3" v-for="( item, index ) in getReversedEpisodeCategory" :key="index" style="margin: 10px" >
-          <button type="button" class="btn btn-secondary" >
+        <div class="col-lg-1 col-md-1 col-sm-2 col-xs-2 col-3" v-for="( item, index ) in paginatedEpisodeList" :key="index" style="margin: 10px" >
+          <button type="button" class="btn btn-secondary" @click="gotoEpisode( item + 1 )" >
              Episode {{item + 1}}
           </button>
         </div>
+        
+        
       </div>
     </div>
   </div>
@@ -36,13 +48,14 @@
         animeInfo: ''
       },
       episodeList: [],
+      paginatedEpisodeList: [],
+      start: 0,
+      end: 20,
       episodeNumber: 0
     }),
 
     computed: {
-      getReversedEpisodeCategory() {
-        return this.episodeList.slice().reverse();
-      }
+    
     },
 
     methods: {
@@ -73,17 +86,41 @@
         this.videoInfo.animeInfo = $('div[class="anime-info"]').find('a').text();
       },
 
+      gotoEpisode( selectedEpisodeNumber ) {
+        this.updateEpisodeNumber();
+        this.$router.push({path: '/watch', query: { title: this.$route.query.title.replace( this.episodeNumber, selectedEpisodeNumber) }});
+      },
+
+      updateEpisodeNumber() {
+        let query = this.$route.query.title.split('-')
+        this.episodeNumber = query[ query.length - 1 ];
+      }
+
      
     },
 
     mounted() {
       this.getVideo();
-      let query = this.$route.query.title.split('-')
-      this.episodeNumber = query[ query.length - 1 ];
+      this.updateEpisodeNumber();
 
       this.episodeList = new Array( parseInt( this.episodeNumber ) );
       this.episodeList = [ ...Array( parseInt( this.episodeNumber ) ).keys() ]
+      this.episodeList = this.episodeList.slice().reverse();
+
+      this.paginatedEpisodeList = this.episodeList.slice( this.start, this.end );
+
+    },
+
+     watch: {
+    $route(to, from) {
+      // react to route changes...
+      if ( to.query != from.query ) {
+        window.location.reload()
+      }
+
+
     }
+  }
 
   }
 </script>
