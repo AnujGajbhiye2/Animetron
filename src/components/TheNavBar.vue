e287dee2b7df984139abf01edd304d70c09b69da
Anuj <anujgajbhiye97@gmail.com>
2021-04-14 01:08:40 +0530

    🐛 fixed responsiveness of navbar

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -2,11 +2,11 @@
   <div>
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="#">Animetron</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" @click="isShow = !isShow">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <div class="collapse navbar-collapse" :class="isShow ? 'show': '' " id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
      <li class="nav-item">
          <router-link class="nav-link" to="/" exact="">Home</router-link>
@@ -27,6 +27,10 @@
 
 <script>
 export default {
+
+  data: () => ({
+    isShow: false
+  })
 
 }
 </script>
